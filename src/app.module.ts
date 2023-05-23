d1c08bdde480ac0b2eb5a59d594a0601829874c3
Martux95 <bastianruizgaray@gmail.com>
2023-05-23 00:13:59 -0400

    :sparkles: Added hexagonal arquitecture modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
+import { AdaptersModule } from './adapters/adapters.module';
+import { ApplicationModule } from './application/application.module';
+
 @Module({
-  imports: [],
+  imports: [InfrastructureModule, ApplicationModule, AdaptersModule],
   controllers: [],
   providers: [],
 })
