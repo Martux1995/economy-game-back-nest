a764be5a1ac54c30f73957cc231d15c162c1b968
Martux95 <bastianruizgaray@gmail.com>
2023-05-31 09:30:02 -0400

    :sparkles: Added CORS Helmet and validation pipes config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(3000);
 }
