b616c5e3c7c581f3e11aba748cbfb7d26a8d7503
Martux95 <bastianruizgaray@gmail.com>
2023-06-05 18:19:35 -0400

    :sparkles: Added RUN validator

---
+++ src/adapters/common/validators/is-chilean-run.ts
@@ -0,0 +1,28 @@
+import {
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  registerDecorator,
+} from 'class-validator';
+import { validateRUN } from '../../../application/common/helpers/run';
+
+@ValidatorConstraint({ name: 'isChileanRUN', async: false })
+class IsChileanRUNConstraint implements ValidatorConstraintInterface {
+  validate(value: any): boolean {
+    return typeof value === 'string' && validateRUN(value);
+  }
+
+  defaultMessage(args?: ValidationArguments): string {
+    return `property ${args.property} should be a valid RUN`;
+  }
+}
+
+export function IsChileanRUN() {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      validator: IsChileanRUNConstraint,
+    });
+  };
+}
