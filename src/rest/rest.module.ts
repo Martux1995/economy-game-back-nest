2cee38654d4778e715caa14d835959efdfe9e62d
Martux95 <bastianruizgaray@gmail.com>
2023-06-13 21:59:26 -0400

    :bug: reactivated routes

--- src/rest/rest.module.ts
+++ src/rest/rest.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
-import { AuthModule } from '../application/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [CommonModule, AuthModule],
-  exports: [AuthModule],
 })
 export class RESTModule {}
