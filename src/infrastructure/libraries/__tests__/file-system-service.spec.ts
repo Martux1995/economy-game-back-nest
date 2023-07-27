c5914b1e4496b5f83dc0fa8e5dbf3219456c48ae
Martux95 <bastianruizgaray@gmail.com>
2023-07-27 01:59:18 -0400

    :test_tube: Added UT

---
+++ src/infrastructure/libraries/__tests__/file-system-service.spec.ts
@@ -0,0 +1,88 @@
+import { Test } from '@nestjs/testing';
+import { FileSystemServiceImp } from '../file-system.service';
+
+import * as fs from 'fs';
+import { EFileType } from '../../../domain/enums';
+import { join } from 'path';
+
+describe('FileSystemServiceImp', () => {
+  let fileSystemService: FileSystemServiceImp;
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [FileSystemServiceImp],
+    }).compile();
+    fileSystemService = module.get<FileSystemServiceImp>(FileSystemServiceImp);
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('getRawFile', () => {
+    it('should get a file', () => {
+      const existsSpyOn = jest.spyOn(fs, 'existsSync').mockReturnValue(true);
+      jest
+        .spyOn(fs, 'readFileSync')
+        .mockImplementation(() => Buffer.alloc(100));
+
+      const result = fileSystemService.getRawFile(
+        'name',
+        EFileType.emailTemplate,
+      );
+
+      expect(result).toBeDefined();
+      expect(result).toStrictEqual<Buffer>(Buffer.alloc(100));
+      expect(existsSpyOn).toBeCalledWith(
+        join(process.cwd(), `./${EFileType.emailTemplate}/name`),
+      );
+    });
+
+    it('should return null if path is invalid', () => {
+      const existsSpyOn = jest.spyOn(fs, 'existsSync').mockReturnValue(false);
+
+      const result = fileSystemService.getRawFile(
+        'name',
+        EFileType.emailTemplate,
+      );
+
+      expect(result).toBeNull();
+      expect(existsSpyOn).toBeCalledWith(
+        join(process.cwd(), `./${EFileType.emailTemplate}/name`),
+      );
+    });
+  });
+
+  describe('getTextFile', () => {
+    it('should get a file', () => {
+      const existsSpyOn = jest.spyOn(fs, 'existsSync').mockReturnValue(true);
+      jest
+        .spyOn(fs, 'readFileSync')
+        .mockImplementation(() => Buffer.alloc(100));
+
+      const result = fileSystemService.getTextFile(
+        'name',
+        EFileType.emailTemplate,
+      );
+
+      expect(result).toBeDefined();
+      expect(result).toStrictEqual<Buffer>(Buffer.alloc(100));
+      expect(existsSpyOn).toBeCalledWith(
+        join(process.cwd(), `./${EFileType.emailTemplate}/name`),
+      );
+    });
+
+    it('should return null if path is invalid', () => {
+      const existsSpyOn = jest.spyOn(fs, 'existsSync').mockReturnValue(false);
+
+      const result = fileSystemService.getTextFile(
+        'name',
+        EFileType.emailTemplate,
+      );
+
+      expect(result).toBeNull();
+      expect(existsSpyOn).toBeCalledWith(
+        join(process.cwd(), `./${EFileType.emailTemplate}/name`),
+      );
+    });
+  });
+});
