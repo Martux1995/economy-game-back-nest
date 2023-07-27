3bc96dd948c043ed2f4ff3099be5d044378be0ab
Martux95 <bastianruizgaray@gmail.com>
2023-07-27 01:42:55 -0400

    :recycle: Simplified read file logic

--- src/infrastructure/libraries/file-system.service.ts
+++ src/infrastructure/libraries/file-system.service.ts
@@ -1,5 +1,6 @@
+import { join } from 'path';
 import { Injectable } from '@nestjs/common';
-import { readFileSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
 
 import { EFileType } from '../../domain/enums';
 import { FileSystemService } from '../../domain/services';
@@ -7,12 +8,14 @@ import { FileSystemService } from '../../domain/services';
 @Injectable()
 export class FileSystemServiceImp extends FileSystemService {
   getRawFile(filename: string, filetype: EFileType): Buffer {
-    return readFileSync(`${__dirname}/../../../../${filetype}/${filename}`);
+    const route = join(process.cwd(), `./${filetype}/${filename}`);
+    return existsSync(route) ? readFileSync(route) : null;
   }
 
   getTextFile(filename: string, filetype: EFileType): string {
-    return readFileSync(`${__dirname}/../../../../${filetype}/${filename}`, {
-      encoding: 'utf-8',
-    });
+    const route = join(process.cwd(), `./${filetype}/${filename}`);
+    return existsSync(route)
+      ? readFileSync(route, { encoding: 'utf-8' })
+      : null;
   }
 }
