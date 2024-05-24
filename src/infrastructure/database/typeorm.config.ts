38d804cde41aa60e8b4c5fc696374a192749bcd9
Martux1995 <bastianruizgaray@gmail.com>
2024-05-24 00:06:28 -0400

    :recycle: Connecting new Config Module to Database Module

--- src/infrastructure/database/typeorm.config.ts
+++ src/infrastructure/database/typeorm.config.ts
@@ -1,14 +1,22 @@
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { EnvService } from '../../domain/services';
+
+import { AppConfig } from '../../config/interfaces/app-config';
+import { EEnvironmentVars } from '../../config/enums/environment-vars.enum';
+
+const BASE_PATH = 'dist/infrastructure/database/';
 
 export const getTypeOrmModuleOptions = (
-  config: EnvService,
+  config: ConfigService<AppConfig>,
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: config.getDatabaseHost(),
-  port: config.getDatabasePort(),
-  username: config.getDatabaseUser(),
-  password: config.getDatabasePassword(),
-  database: config.getDatabaseName(),
+  host: config.get(EEnvironmentVars.DATABASE_HOST),
+  port: config.get(EEnvironmentVars.DATABASE_PORT),
+  username: config.get(EEnvironmentVars.DATABASE_USER),
+  password: config.get(EEnvironmentVars.DATABASE_PASS),
+  database: config.get(EEnvironmentVars.DATABASE_NAME),
+  entities: [`${BASE_PATH}entities/*.entity.js`],
+  migrations: [`${BASE_PATH}migrations/*.js`],
   autoLoadEntities: true,
+  synchronize: true,
 });
